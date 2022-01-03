652a69bfe4b01d831f8b43323397a72308d4c28c
Luisana Perez <luisanahenmaryp@gmail.com>
2022-01-03 18:02:23 -0500

    cree el componente 'Contenedor'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import styled from 'styled-components'
+
+const Contenedor = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
+
 const App = () =>{
   return(
 
-    <div>hola</div>
+    <Contenedor>HOLA MUNDO</Contenedor>
   )
 }
 
